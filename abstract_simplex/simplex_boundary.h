65f8b8f224d9762522a1d46adf4bbcb88d68b248
Ryan H. Lewis <me@ryanlewis.net>
2013-08-07 20:34:12 -0600

    simplex_boundary

--- abstract_simplex/simplex_boundary.h
+++ abstract_simplex/simplex_boundary.h
@@ -2,23 +2,6 @@
 #define SIMPLEX_BOUNDARY_H
 #include "term.h"
 
-namespace ct { 
-template< typename Simplex_, typename Coefficient, 
-	  typename Term_< typename Simplex_, typename Coefficient > >
-class Simplex_boundary {
-public:
-	typedef Simplex_ Simplex;
-	typedef Term_< Simplex, Coefficient> Term;
-	typedef const_boundary_iterator< Term> const_iterator;
-	//default constructor
-	Simplex_boundary(){};	
-
-	//It only makes sense for const iterators
-	const_iterator begin( const Simplex & s){ return const_iterator( s); }
-	const_iterator end( const Simplex & s){};
-}; //Simplex_boundary
-}
-
 //non-exported functionality
 namespace {
 
@@ -30,31 +13,32 @@ class const_boundary_iterator :
 			      const Term_*,
 			      const Term_>{
 	private:
-	  typedef typename const_boundary_iterator< Term_> Self;
+	  typedef const_boundary_iterator< Term_> Self;
 	  typedef Term_ Term;
 	  typedef typename Term::Cell Cell;
 	  typedef typename Cell::value_type Vertex;
 	public:
 	  //default constructor
 	  const_boundary_iterator(): cellptr( 0), pos( 0){}
-	  const_boundary_iterator( const Simplex & s): pos( 0){
+	  const_boundary_iterator( const Cell & s): pos( 0){
 	  	if( s.dimension()){
 		    //begin by removing first vertex
 		    cellptr = &s;
 		    removed = s.vertices[ 0];
-		    face.get_cell().vertices.resize( s.dimension());
+		    face.cell().vertices.resize( s.dimension());
 		    face.set_coefficient( 1);
-		    std::copy( s.begin()+1, s.end(), face_.get_cell().begin());
-		}else{ cell_ = 0; } //\partial(vertex) = 0
+		    std::copy( s.begin()+1, s.end(), face.cell().begin());
+		}else{ cellptr = 0; } //\partial(vertex) = 0
 	  }
 	  //copy constructor
-	  const_boundary_iterator( const Self & from) cellptr( from.cellptr), 
+	  const_boundary_iterator( const Self & from): cellptr( from.cellptr), 
 	  face(from.face), pos( from.pos), removed( from.removed){}
-	  const_boundary_iterator=( const_boundary_iterator & from){
+	  const_boundary_iterator& operator=( const_boundary_iterator & from){
 	  	cellptr = from.cellptr;
 		pos = from.pos;
 		face = from.face;
 		removed = from.removed;
+		return &(*this);
 	 }
 	 const Term& operator*() const {
 	 	if( cellptr){ return face; }
@@ -71,12 +55,12 @@ class const_boundary_iterator :
 	 }
 
 	 const_boundary_iterator& operator++(){
-		if( pos == face.get_cell().size()){
+		if( pos == face.cell().size()){
 			cellptr = 0;
 			pos = 0;
 		}
 		//return removed vertex, get rid of another one
-		std::swap( face.get_cell().vertices[ pos++], removed);
+		std::swap( face.cell().vertices[ pos++], removed);
 		face.set_coefficient( -face.get_coefficient());
 		return *this;	
 	 }
@@ -89,14 +73,29 @@ class const_boundary_iterator :
 
 	 private:
 		const Cell* cellptr;
-		Cell::size_type pos;
+		typename Cell::size_type pos;
 		Term face;
 		Vertex removed;
 			
-	} // END const_boundary_iterator
+	}; // END const_boundary_iterator
 } //END private namespace
 
+namespace ct { 
+template< typename Simplex_, typename Coefficient, 
+	  template<typename, typename> class Term_ >
+class Simplex_boundary {
+public:
+	typedef Simplex_ Simplex;
+	typedef Term_< Simplex, Coefficient> Term;
+	typedef const_boundary_iterator< Term> const_iterator;
+	//default constructor
+	Simplex_boundary(){};	
 
-}
+	//It only makes sense for const iterators
+	const_iterator begin( const Simplex & s){ return const_iterator( s); }
+	const_iterator end( const Simplex & s){};
+}; //Simplex_boundary
+
+} // end namespace ct
 
 #endif //SIMPLEX_BOUNDARY_H
