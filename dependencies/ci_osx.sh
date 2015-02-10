b605ab3acb7e3a493305acf6f647830f3f520008
Ryan H. Lewis <me@ryanlewis.net>
2015-02-09 18:11:32 -0800

    travis osx builds modified..

--- dependencies/ci_osx.sh
+++ dependencies/ci_osx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-brew install tbb metis doxygen openmpi ann 
-brew install boost ---build-from-source --with-c++11 --with-mpi --with-program_options --with-clang --without-single
+brew tap homebrew/science
+brew install tbb metis doxygen openmpi ann
+#brew install boost ---build-from-source --with-c++11 --with-mpi --with-program_options --with-clang --without-single
