106be8bc25be46c3e068f3a51c3ce222f0f92e35
Ryan H. Lewis <me@ryanlewis.net>
2015-02-09 18:14:56 -0800

    ci_os.sh only supports one compiler per OS..

--- dependencies/ci_linux.sh
+++ dependencies/ci_linux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#we do not support clang on osx..
+CC=gcc
+CXX=g++
 sudo add-apt-repository ppa:yade-users/external -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get update -qq  
--- dependencies/ci_osx.sh
+++ dependencies/ci_osx.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+CC=clang
+CXX=clang++
 brew tap homebrew/science
 brew install tbb metis doxygen openmpi ann
 #brew install boost ---build-from-source --with-c++11 --with-mpi --with-program_options --with-clang --without-single
