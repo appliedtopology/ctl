9cdc6c55b73fad1f2f8649059ce1c6967f086ef2
Ryan H. Lewis <me@ryanlewis.net>
2015-02-14 13:42:05 -0800

    -y needed.

--- dependencies/ci_linux.sh
+++ dependencies/ci_linux.sh
@@ -2,7 +2,7 @@
 #we do not support clang on osx..
 
 #These are all hacks to get stuff not available on Ubuntu 12.xx
-sudo add-apt-repository ppa:libreoffice/ppa
+sudo add-apt-repository ppa:libreoffice/ppa -y
 sudo add-apt-repository ppa:yade-users/external -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get clean
