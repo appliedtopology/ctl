28a80c63783a3c6070439d6258e8b42565432620
Ryan H. Lewis <me@ryanlewis.net>
2015-02-13 22:20:16 -0800

    fix ubuntu build typo and ci builds faster.

--- dependencies/ci_linux.sh
+++ dependencies/ci_linux.sh
@@ -9,4 +9,4 @@ if [ "$CXX" == "g++" ]; then sudo apt-get install -qq g++-4.8; fi
 if [ "$CXX" == "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8 
 sudo update-alternatives --config gcc 
-sudo apt-get install doxygen libann-dev libboost-all-dev libtbb-dev libmetis-dev 
+sudo apt-get install libann-dev libboost-all-dev libtbb-dev libmetis-dev 
