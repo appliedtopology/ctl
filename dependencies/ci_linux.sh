6a25e9d0ee00a11a07d798d866a6ab2d0276e4a5
Ryan H. Lewis <me@ryanlewis.net>
2015-02-14 13:37:51 -0800

    boost still failing.

--- dependencies/ci_linux.sh
+++ dependencies/ci_linux.sh
@@ -2,12 +2,14 @@
 #we do not support clang on osx..
 sudo add-apt-repository ppa:yade-users/external -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
+sudo apt-get clean
 sudo apt-get update -qq  
+sudo apt-get update
 if [ "$CXX" == "g++" ]; then 
 	sudo apt-get install -qq g++-4.8; 
 	export CXX="g++-4.8" CC="gcc-4.8"; 
          sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.8 
          sudo update-alternatives --config gcc 
 fi
-sudo apt-cache search *boost*
+sudo apt-cache search `*boost*`
 sudo apt-get install libann-dev libboost1.54-all-dev libtbb-dev libmetis-dev 
