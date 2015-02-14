2df5f57ae4f8cfbe143a571e0c6c65d4bb7925e7
Ryan H. Lewis <me@ryanlewis.net>
2015-02-14 14:42:59 -0800

    clang on ubuntu 12.xx is broken. giving up

--- dependencies/ci_linux.sh
+++ dependencies/ci_linux.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#we do not support clang on osx..
-
+#we do not support clang on ubuntu 12.x..
+CC=gcc
+CXX=g++
 #These are all hacks to get stuff not available on Ubuntu 12.xx
 sudo add-apt-repository ppa:libreoffice/ppa -y
 sudo add-apt-repository ppa:yade-users/external -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y 
 sudo apt-get clean
 sudo apt-get update -qq  
-sudo apt-get update
 if [ "$CXX" == "g++" ]; then 
 	sudo apt-get install -qq g++-4.8; 
 	export CXX="g++-4.8" CC="gcc-4.8"; 
