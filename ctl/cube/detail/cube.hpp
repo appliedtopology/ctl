0387049bb48126e858f0ed0a1948df873010dfdf
Ryan H. Lewis <me@ryanlewis.net>
2015-03-30 19:19:14 -0700

    bsd license headers.

--- ctl/cube/detail/cube.hpp
+++ ctl/cube/detail/cube.hpp
@@ -18,6 +18,7 @@
 * !!! DO NOT CITE THE USER MANUAL !!!
 *******************************************************************************
 * Copyright (C) Ryan H. Lewis 2014 <me@ryanlewis.net>
+* Released under BSD-3 License. See LICENSE
 *******************************************************************************
 * Notes: 
 * August 27, 2014
