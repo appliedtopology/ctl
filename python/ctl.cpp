8be2ff358537265fdd72f96d5bc98813f885c042
Ryan H. Lewis <me@ryanlewis.net>
2016-04-12 10:11:49 -0700

    compute homology in python.

--- python/ctl.cpp
+++ python/ctl.cpp
@@ -14,19 +14,17 @@
 namespace py = pybind11;
 
 #include "wrap_simplex.hpp"
-#include "wrap_boundary.hpp"
+//#include "wrap_boundary.hpp"
 //#include "wrap_complex.hpp"
-
+#include "wrap_persistence.hpp"
 //## Define the module
-//This is where we actually define the `_phat` module. We'll also have a `phat` module that's written
-//in Python, which will use `_phat` as an implementation detail.
-PYBIND11_PLUGIN(_ctl) {
+//This is where we actually define the `ctl` module. We'll also have a `phat` module that's written
+PYBIND11_PLUGIN(ctl) {
   //Create the module object. First arg is the name, second is the module docstring.
-  py::module m("ctl", "C++ wrapper for CTL. Please use the CTL module, not the _ctl module");
-
+  py::module m("ctl", "C++ wrapper for CTL.");
   //Wrap the `persistence_pairs` class
   wrap_simplex(m);
-
+  wrap_persistence(m);
   //We're done!
   return m.ptr();
 
