34d4ef6a1f0fc1bfb946babeaafa5c19e50a66d9
Ryan H. Lewis <me@ryanlewis.net>
2016-04-27 14:01:35 -0700

    product cell.

---
+++ ctl/product_cell/product_boundary.hpp
@@ -0,0 +1,149 @@
+#ifndef PRODUCT_BOUNDARY_H
+#define PRODUCT_BOUNDARY_H
+/*******************************************************************************
+* -Academic Honesty-
+*******************************************************************************
+* Copyright (C) Ryan H. Lewis 2014 <me@ryanlewis.net>
+*******************************************************************************
+* ********** BSD-3 License ****************
+*******************************************************************************/
+//STL 
+#include <cassert>
+
+//CTL
+#include <ctl/term/term.hpp>
+#include <ctl/finite_field/finite_field.hpp>
+
+//non-exported functionality
+namespace {
+//TODO: Concept_check Boundary1_ and Boundary2_ should have the same coefficient 
+template< typename Term_, typename Boundary1_, typename Boundary2_, 
+	   typename Coefficient_>
+class const_product_boundary_iterator : 
+	public std::iterator< std::bidirectional_iterator_tag,
+			      Term_,
+			      std::ptrdiff_t,
+			      const Term_*,
+			      const Term_>{
+	private:
+	  typedef Boundary1_ Boundary1; 
+	  typedef Boundary2_ Boundary2; 
+	  typedef Term_ Term;
+	  typedef Coefficient_ Coefficient;
+	  typedef typename Term::Cell Product;
+	  typedef const_product_boundary_iterator< Term_, Boundary1_, 
+					   Boundary2_, Coefficient_> Self;
+	  typedef typename Boundary1::const_iterator const_iterator1;
+	  typedef typename Boundary2::const_iterator const_iterator2;
+	public:
+	//default constructor
+	const_product_boundary_iterator() {}
+
+	//begin constructor
+	const_product_boundary_iterator( const Boundary1& b1, const Boundary2& b2, 
+				 const Product & p): 
+		cellptr( &p),
+		face1( b1.begin( p.first)),
+		face2( b2.begin( p.second)),
+		end1( b1.end( p.first)),
+		end2( b2.end( p.second)),
+		//even --> 1 and odd --> -1
+		sign( 2*(p.first_cell().dimension()%2!=0)-1){ next_term(); }
+
+	const_product_boundary_iterator( const Boundary1& b1, 
+					 const Boundary2& b2, 
+					 const Product & p, const bool b): 
+		end1( b1.end( p.first)),
+		end2( b2.end( p.second)),
+		sign( 2*(p.first_cell().dimension()%2!=0)-1){ end_term(); }
+	
+	Term& operator*() { return face; }
+	Term* operator->() { return &face; }
+	
+	//equality
+	bool operator==( const const_product_boundary_iterator & b) const { 
+		return (b.face == face); 
+	}
+	bool operator!=( const const_product_boundary_iterator & b) const { 
+		return (b.face != face); 
+	}
+
+	const_product_boundary_iterator& operator++(){
+		next_term();
+		return *this;	
+	}
+
+	const_product_boundary_iterator operator++( int){
+	 	const_product_boundary_iterator tmp( *this); 
+		++(*this); //now call previous operator
+		return tmp;
+	}
+	protected: 
+	void next_term(){
+		//\partial(\sigma) \times \tau 
+		if( face1 != end1){
+			face.cell().first = face1->cell();
+			face.cell().second = cellptr->second;
+			face.coefficient( face1->coefficient()); 
+			++face1;
+		} else if ( face2 != end2){
+			face.cell().first = cellptr->first;
+			face.cell().second = face2->cell();
+			face.coefficient( sign*face2->coefficient()); 
+			++face2;
+		} else {
+			end_term();
+		}
+	}
+	void end_term(){
+		face.cell().first  = end1->cell();
+		face.cell().second = end2->cell();
+		face.coefficient( 1);
+	}
+	private:
+	 const Product* cellptr;
+	 const_iterator1 face1;
+	 const_iterator2 face2;
+	 const_iterator1 end1;
+	 const_iterator2 end2;
+	 int sign=0;
+	 Term face;
+}; // END const_product_boundary_iterator
+} //END private namespace
+
+namespace ctl { 
+template< typename Product_, typename Boundary1_, typename Boundary2_, 
+	  typename Coefficient = typename Boundary1_::Coefficient>
+class Product_boundary {
+public:
+	typedef Product_ Product;
+	typedef Product Cell;
+	typedef Boundary1_ Boundary1;
+	typedef Boundary2_ Boundary2;
+	typedef typename Boundary1::Term::template 
+			rebind< Product, Coefficient>::T Term;
+	typedef const_product_boundary_iterator< Term, Boundary1, Boundary2, Coefficient> const_iterator;
+	//initialization constructor
+	Product_boundary( const Boundary1 & b1, const Boundary2 & b2): 
+	bd1( b1), bd2( b2) {};	
+
+	//It only makes sense for const iterators
+	const_iterator begin( const Product & p) const { 
+		return const_iterator( bd1, bd2, p); 
+	}
+	const_iterator end( const Product & p) const   { 
+		return const_iterator( bd1, bd2, p, true); 
+	}
+	std::size_t length( const Product & p) const { 
+		return bd1.length( p.first) + bd2.length( p.second); 
+	}
+	const Boundary1& boundary1() const { return bd1; }
+	const Boundary2& boundary2() const { return bd2; }
+	private:
+	const Boundary1 bd1;
+	const Boundary2 bd2;
+}; //Product_boundary
+
+} // namespace ctl
+
+#endif //PRODUCT_BOUNDARY_H
---
+++ ctl/product_cell/product_cell.hpp
@@ -0,0 +1,116 @@
+#ifndef PRODUCT_CELL_H
+#define PRODUCT_CELL_H
+/*******************************************************************************
+* Copyright (C) Ryan H. Lewis 2014 <me@ryanlewis.net>
+*******************************************************************************
+* ********** BSD-3 License ****************
+******************************************************************************/
+/* NOTES:
+* This product cell design is a pair< Cell1, Cell2>
+* For space concerns, we represent Cell1 and Cell2 as _iterators_
+* - Of course, stylistically, it would be good for Product_cell, to allow for Cell1/Cell2 to truly be 
+    the value type of these iterators
+*/
+#include <utility> //std::pair, and piecewise_construct
+#include <tuple> //std::pair, and piecewise_construct
+#include <iostream> //cout (debug only)
+#include <ctl/product_cell/product_boundary.hpp>
+#include <ctl/hash/hash.hpp>
+
+namespace ctl {
+
+template< typename Cell1_, typename Cell2_>
+class Product_cell : public std::pair< Cell1_ , Cell2_> {
+	public:	
+	typedef Cell1_ Cell1;
+	typedef Cell2_ Cell2;
+	private:
+	typedef std::pair< Cell1, Cell2> Pair;
+	typedef Product_cell< Cell1, Cell2> Self;
+	public:
+
+	//default
+	Product_cell() {};
+
+	//copy
+	Product_cell( const Product_cell & from) : Pair( from) {}	
+
+	//standard
+	Product_cell( const Cell1 & cell1, 
+		      const Cell2 & cell2): Pair( cell1, cell2) {}
+	//initialize
+	Product_cell( Cell1 && cell1, Cell2 && cell2): Pair( std::forward(cell1), std::forward(cell2)) {}
+	//move
+	Product_cell( Product_cell && from): Pair( std::forward< Pair>( from)){}
+
+	//piecewise
+	template< class... Args1, class... Args2>
+	Product_cell( std::piecewise_construct_t pwc, 
+		      std::tuple< Args1...> first_args, 
+		       std::tuple<Args2...> second_args):
+	Pair( pwc, first_args, second_args) {}
+
+	const Cell1& first_cell() const  { return this->first;  }	
+	const Cell2& second_cell() const { return this->second; }	
+	std::size_t  dimension() const	{ return first_cell().dimension() + second_cell().dimension(); }
+
+	//Assignment
+	Self& operator=( const Self & b) { 
+		Pair::operator=( b); 
+		return *this;
+	}
+
+	//Move assignment
+	Self& operator=( Self && b) {
+		Pair::operator=( std::forward( b));
+		return *this;
+	}
+
+	//Comparisons.	
+	bool operator<( const Self & b) const {
+		if (this->first == b.first) { 
+			return second_cell() < b.second_cell();
+		}
+		return first_cell() < b.first_cell();
+	}
+	
+	bool operator==( const Self & b) const { return (this->first == b.first) && (this->second == b.second); }
+	bool operator!=( const Self & b) const { return (this->first != b.first) || (this->second != b.second); }
+
+	std::size_t hash() const{
+		constexpr std::size_t shift_factor = sizeof(std::size_t) >> 2;
+		return ctl::hash_function( first_cell()) |
+			(ctl::hash_function( second_cell()) << shift_factor);
+	}
+
+	struct Hash {
+  	   std::size_t operator()( const Product_cell & p) const {
+  		  return p.hash();
+  	   }
+	};
+	//File I/O
+	template< typename Stream>
+	void write( Stream & out) const {
+		first_cell().write( out);
+		out << ctl::otimes;
+		second_cell().write( out);
+	}
+
+}; //Product_cell
+
+template< typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const Product_cell< T1, T2> && alpha){
+	out << alpha.first_cell() << ctl::otimes 
+	    << alpha.second_cell();
+	return out;
+}
+template< typename T1, typename T2>
+std::ostream& operator<<(std::ostream& out, const Product_cell< T1, T2> & alpha){
+	out << alpha.first_cell() << ctl::otimes 
+	    << alpha.second_cell();
+	return out;
+}
+
+} // namespace ctl
+
+#endif //PRODUCT_CELL_H
